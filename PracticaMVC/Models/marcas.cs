b6a594f39b92f631abd001bcdb0849be88f04314
Juan José <juanjosemagaa@hotmail.com>
2025-03-07 17:52:29 -0600

    Practica 5 MVC Vistas

---
+++ PracticaMVC/Models/marcas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PracticaMVC.Models
+{
+    public class marcas
+    {
+        [Key]
+        [Display(Name ="ID")]
+        public int id_marcas { get; set; }
+        [Required(ErrorMessage ="El nombre de la marca NO es opcional")]
+        [Display(Name ="Nombre de la Marca")]
+        public string? nombre_marca { get; set; }
+        [Display(Name ="Estado")]
+        [StringLength(1,ErrorMessage ="La cantidad maxima de caracteres valida es {1}")]
+        public string? estados { get; set; }
+    }
+}
--- PracticaMVC/Program.cs
+++ PracticaMVC/Program.cs
@@ -1,7 +1,13 @@
+using PracticaMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<equiposDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("equiposDbConnection")));
 
 var app = builder.Build();
 
@@ -22,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=marcas}/{action=Index}/{id?}");
 
 app.Run();
